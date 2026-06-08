6d11341138e9bc040b3e49fe7c55bb93a7ef3d92
koyanagimui <koyanagimui@compass-japan.com>
2026-06-08 09:09:12 +0900

    first commit

---
+++ src/main/java/com/example/cicd_training/CicdTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicd_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cicd_training/CicdTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
